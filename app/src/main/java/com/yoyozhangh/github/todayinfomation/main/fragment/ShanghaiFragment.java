1b128c081a7d25cea96aff7cb18ec8a4956ee9b5
haiyun <131376144@qq.com>
2020-02-29 18:04:02 +0800

    design 套件 CoordinatorLayout 协调布局如何实现嵌套滚动

--- app/src/main/java/com/yoyozhangh/github/todayinfomation/main/fragment/ShanghaiFragment.java
+++ app/src/main/java/com/yoyozhangh/github/todayinfomation/main/fragment/ShanghaiFragment.java
@@ -1,14 +1,26 @@
 package com.yoyozhangh.github.todayinfomation.main.fragment;
 
 
-import androidx.fragment.app.Fragment;
+import android.widget.TextView;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.yoyozhangh.github.todayinfomation.R;
 import com.yoyozhangh.github.todayinfomation.base.BaseFragment;
 import com.yoyozhangh.github.todayinfomation.base.ViewInject;
 
+import butterknife.BindView;
+
 @ViewInject(mainlayoutid = R.layout.fragment_shanghai)
 public class ShanghaiFragment extends BaseFragment {
+
+
+    @BindView(R.id.tv_shanghai_welcome)
+    TextView tvShanghaiWelcome;
+    @BindView(R.id.shanghai_collapsingtoolbarlayout)
+    CollapsingToolbarLayout shanghaiCollapsingtoolbarlayout;
+    @BindView(R.id.shanghai_app_barlayout)
+    AppBarLayout shanghaiAppBarlayout;
 
     @Override
     public void afterBindView() {
