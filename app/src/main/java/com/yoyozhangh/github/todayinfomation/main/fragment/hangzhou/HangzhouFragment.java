9645e08a562583cb98262cf403900d5d163abbe4
haiyun <131376144@qq.com>
2020-03-14 09:06:55 +0800

    ViewPager的高级使用 动态更新，内存优化 懒加载

--- app/src/main/java/com/yoyozhangh/github/todayinfomation/main/fragment/hangzhou/HangzhouFragment.java
+++ app/src/main/java/com/yoyozhangh/github/todayinfomation/main/fragment/hangzhou/HangzhouFragment.java
@@ -1,18 +1,96 @@
 package com.yoyozhangh.github.todayinfomation.main.fragment.hangzhou;
 
 
-import androidx.fragment.app.Fragment;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 import com.yoyozhangh.github.todayinfomation.R;
 import com.yoyozhangh.github.todayinfomation.base.BaseFragment;
 import com.yoyozhangh.github.todayinfomation.base.ViewInject;
+import com.yoyozhangh.github.todayinfomation.main.fragment.shenzhen.ShenzhenFragment;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
 
 
 @ViewInject(mainlayoutid = R.layout.fragment_hangzhou)
 public class HangzhouFragment extends BaseFragment {
 
+    @BindView(R.id.tl_tablayout)
+    TabLayout tlTablayout;
+    @BindView(R.id.vp_viewpager)
+    ViewPager vpViewpager;
+
+//    ArrayList<String> arrayList = new ArrayList<>();
+
     @Override
     public void afterBindView() {
+//
+//        for (int i = 0; i < 5; i++) {
+//            arrayList.add("深圳");
+//        }
 
+        tlTablayout.setupWithViewPager(vpViewpager);
+
+//        vpViewpager.setAdapter(new PagerAdapter() {
+//            @Override
+//            public int getCount() {
+//                return 0;
+//            }
+//
+//            @Override
+//            public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+//                return false;
+//            }
+//        });
+
+        vpViewpager.setAdapter(new FragmentStatePagerAdapter(getChildFragmentManager()) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                Log.e("HangzhouFragment", "getItem: position=" + position);
+
+                //根据不同的position 分别new 不同的fragment
+//                switch (position){
+//                    case 
+//                }
+
+                return new ShenzhenFragment();
+            }
+
+            @Override
+            public int getCount() {
+//                return arrayList.size();
+                return 5;
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+//                return arrayList.get(position);
+                return "深圳";
+            }
+        });
+
+//        vpViewpager.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                arrayList.add("深圳");
+//                vpViewpager.getAdapter().notifyDataSetChanged();
+//            }
+//        },5000);
+//        // 预加载多少条目到内存中
+//        vpViewpager.setOffscreenPageLimit(0);
+
+        //FragmentPagerAdapter 会走 onPause onDestroyView
+        //FragmentStatePagerAdapter 会走 onPause onDestroyView onDestroy  onDetach
     }
 }
--- app/src/main/java/com/yoyozhangh/github/todayinfomation/main/fragment/shenzhen/ShenzhenFragment.java
+++ app/src/main/java/com/yoyozhangh/github/todayinfomation/main/fragment/shenzhen/ShenzhenFragment.java
@@ -1,6 +1,12 @@
 package com.yoyozhangh.github.todayinfomation.main.fragment.shenzhen;
 
 
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
 import com.yoyozhangh.github.todayinfomation.R;
 import com.yoyozhangh.github.todayinfomation.base.BaseFragment;
 import com.yoyozhangh.github.todayinfomation.base.ViewInject;
@@ -11,5 +17,51 @@ public class ShenzhenFragment extends BaseFragment {
     @Override
     public void afterBindView() {
 
+    }
+
+//    @Override
+//    public void setUserVisibleHint(boolean isVisibleToUser) {
+//        if (isVisibleToUser){
+//            // 加载网络数据
+//        }
+//        super.setUserVisibleHint(isVisibleToUser);
+//    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.e("ShenzhenFragment", "onAttach: " );
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e("ShenzhenFragment", "onCreate: " );
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.e("ShenzhenFragment", "onPause: " );
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.e("ShenzhenFragment", "onDestroy: " );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.e("ShenzhenFragment", "onDestroyView: " );
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.e("ShenzhenFragment", "onDetach: " );
     }
 }
